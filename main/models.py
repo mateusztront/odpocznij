1e307d7299b599c65abacbcce463c2ca0d29d7ba
mateusztront <mateusztront@gmail.com>
2021-03-08 17:35:47 +0100

    models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+TYPES = [
+    ('ZOL', 'Zakład opiekuńczo-leczniczy'),
+    ('OR', 'Oddział Rehabilitacji'),
+    ('S', 'Sanatorium'),
+    ('DPS', 'Dom Pomocy Społecznej'),
+    ('DOP', 'Długoterminowa opieka prywatna'),
+    ('KOP', 'Krótkoterminowa opieka prywatna'),
+]
+
+
+class Premises(models.Model):
+    name = models.CharField(max_length=256)
+    address = models.CharField("Adres", max_length=1024)
+    zip_code = models.CharField("Kod pocztowy", max_length=12)
+    city = models.CharField("Miejscowość", max_length=1024)
+    country = CountryField(COUNTRIES_FIRST='PL')
+    type = models.CharField(max_length=32, choices=TYPES)
+
+
+class Room(models.Model):
+    people_number = models.IntegerField()
+    capacity = models.FloatField(null=True, blank=True)
+    premises = models.ForeignKey(Premises)
+
+
+class Feature(models.Model):
+    name = models.CharField(max_length=256)
+    room = models.ManyToManyField(Room)
